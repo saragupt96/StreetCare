172b6d058c59e4a6db551386d18b7bdf4e62ab6a
Micahel Thornton <michael.s.thornton@gmail.com>
2022-07-19 13:11:12 -0400

    adding root screens

--- app/src/main/java/org/brightmindenrichment/street_care/MainActivity.kt
+++ app/src/main/java/org/brightmindenrichment/street_care/MainActivity.kt
@@ -26,10 +26,7 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.appBarMain.toolbar)
 
-        binding.appBarMain.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
+
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_content_main)
@@ -37,7 +34,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_visit, R.id.nav_user
+                R.id.nav_home, R.id.nav_visit, R.id.nav_community, R.id.nav_user
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
