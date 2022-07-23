5c2cce27a5a86843b892917428fd34bd0f833b75
Anupama Baiswar <anupama.baiswar@gmail.com>
2022-07-22 23:57:12 -0700

    starting work on home screen

--- app/src/main/java/org/brightmindenrichment/street_care/ui/home/HomeFragment.kt
+++ app/src/main/java/org/brightmindenrichment/street_care/ui/home/HomeFragment.kt
@@ -5,8 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
+import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.slider.Slider
+import com.smarteist.autoimageslider.SliderView
+import org.brightmindenrichment.street_care.R
 import org.brightmindenrichment.street_care.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -16,6 +21,10 @@ class HomeFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+    private val images = arrayOf<Int>(R.drawable.image12,R.drawable.image16,R.drawable.image25)
+    lateinit var sliderView:SliderView
+    lateinit var cardHowToHelp: CardView
+    var sliderAdapter: SliderAdapter = SliderAdapter(images)
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -31,8 +40,26 @@ class HomeFragment : Fragment() {
         return root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setUpSliderImagesView(view)
+        cardHowToHelp = view.findViewById(R.id.card_how_to_help)
+
+
+    }
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+    private fun setUpSliderImagesView(view : View){
+        sliderView = view.findViewById(R.id.slider_view)
+        sliderView.setSliderAdapter(sliderAdapter)
+        sliderView.autoCycleDirection = Slider.LAYOUT_DIRECTION_LTR
+        sliderView.isAutoCycle= true
+        sliderView.scrollTimeInSec =2
+        sliderView.startAutoCycle()
+    }
+
 }
+
+
